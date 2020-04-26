cad5dcfa0c29a090e89a3d15f03e89480872ac0b
Kir <kir.dontsov@gmail.com>
2020-04-26 23:32:57 +0300

    seo

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -13,6 +13,8 @@ export const Layout: FC<Props> = ({ children, title, description }) => (
 		<Head>
 			<title>{title}</title>
 			<meta name="description" content={description} />
+			<meta name="yandex-verification" content="9fcaa5f92320287d" />
+			<meta name="google-site-verification" content="4GFebNPOpe9lCdvQkf9pXJGj5IWEBkxcGE9J736ZDOY" />
 			<meta charSet="utf-8" />
 			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 			<link rel="icon" type="image/x-icon" href={require("../assets/img/logo192.png")} />
