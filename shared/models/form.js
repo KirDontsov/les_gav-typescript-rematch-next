09bb07a20696e468f2e582711383efc50db12eb9
Kir <kir.dontsov@gmail.com>
2020-04-28 15:37:15 +0300

    php

--- shared/models/form.js
+++ shared/models/form.js
@@ -32,7 +32,7 @@ export const form = {
 		changeDisabled(payload, state) {
 			if (state.form.phone && state.form.email && state.form.email.includes("@") !== "") {
 				dispatch.form.setFetching(true);
-				fetch("/api/send-email", {
+				fetch("http://297570-cs09732.tmweb.ru", {
 					method: "POST",
 					body: JSON.stringify({
 						email: state.form.email,
@@ -45,33 +45,7 @@ export const form = {
 				}).then(response => {
 					dispatch.form.setFetching(false);
 					dispatch.form.setRes(true);
-					console.log("Successful" + response);
 				});
-				const SENDGRID_API_KEY = "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";
-				const sgMail = require("@sendgrid/mail");
-				sgMail.setApiKey(SENDGRID_API_KEY);
-				const msg = {
-					to: "kir.dontsov@gmail.com",
-					from: "test@example.com",
-					subject: "Sending with Twilio SendGrid is Fun",
-					text: JSON.stringify({
-						email: state.form.email,
-						phone: state.form.phone,
-						name: state.form.name
-					}),
-					html: "<strong>and easy to do anywhere, even with Node.js</strong>"
-				};
-				(async () => {
-					try {
-						await sgMail.send(msg);
-					} catch (error) {
-						console.error(error);
-
-						if (error.response) {
-							console.error(error.response.body);
-						}
-					}
-				})();
 			}
 		}
 	})
