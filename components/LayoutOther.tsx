e70859d84e770bf8f8239f028e1d4c83981aa29e
Kir <kir.dontsov@gmail.com>
2020-05-07 11:14:03 +0300

    gtm

--- components/LayoutOther.tsx
+++ components/LayoutOther.tsx
@@ -16,9 +16,24 @@ export const LayoutOther: FC<Props> = ({ children, title, description }) => (
 			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 			<link rel="icon" type="image/x-icon" href={require("../assets/img/logo192.png")} />
 			<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/leaflet.css" />
+			<script
+				dangerouslySetInnerHTML={{
+					__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+					'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+					})(window,document,'script','dataLayer','GTM-5WW4MRH');`
+				}}
+			/>
 		</Head>
 
 		{children}
 		<Footer />
+		<noscript
+			dangerouslySetInnerHTML={{
+				__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5WW4MRH"
+				height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+			}}
+		/>
 	</div>
 );
