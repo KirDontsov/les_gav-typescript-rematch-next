557de3b2b1f6f6a5762f8df9d95c661f41a823c4
Kir <kir.dontsov@gmail.com>
2020-05-07 11:17:00 +0300

    gtm2

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,19 +1,11 @@
 import App from "next/app";
 import React from "react";
-import TagManager from "react-gtm-module";
 
 import withRematch from "../shared/withRematch";
 import { Provider } from "react-redux";
 import "../styles/index.scss";
 
-const tagManagerArgs = {
-	id: "GTM-5WW4MRH"
-};
-
 class MyApp extends App {
-	componentDidMount() {
-		TagManager.initialize(tagManagerArgs);
-	}
 	render() {
 		const { Component, pageProps, reduxStore } = this.props;
 		return (
